7a8a6c5038458d57e81065234c7a8b92b1bab291
BaltiiSour <baltazardiaz1@hotmail.com>
2019-12-03 17:44:36 -0300

    Update settings.py

--- dailyhub/settings.py
+++ dailyhub/settings.py
@@ -56,8 +56,8 @@ ROOT_URLCONF = 'dailyhub.urls'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sa.adobeacc@gmail.com'
-EMAIL_HOST_PASSWORD = 'ususc2hu'
+EMAIL_HOST_USER = 'user@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'DailyHUB <user@gmail.com>'
 
